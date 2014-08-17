df50465b2ac83e68337fe19c9275d297baee056a
Luigi Vincent <luigivincenthr@gmail.com>
2014-08-17 16:45:02 -0400

    Update Function.java

--- Function.java
+++ Function.java
@@ -10,13 +10,30 @@ public class Function {
   public static String reverse(String str) {  
     return new StringBuilder(str).reverse().toString();
   }
-  // add up all the numbers from 1 to num
+  // Add up all the numbers from 1 to num
   public static int addTill(int num) {
     return num * (num + 1) / 2;
   }
-  // check if a word is a palindrome
+  // Check if a word is a palindrome
   public static boolean isPalindrome(String str) {
   	return str.equals(new StringBuilder(str).reverse().toString());
+  }
+  // Count the vowels in a string
+  public static int vowelCount(String str) {
+  	int count = 0;
+  	if (str.isEmpty()) return count;
+  	for (int i = 0; i < str.length(); i++){
+  		switch(str.charAt(i)) {
+  			case 'a':
+  			case 'e':
+  			case 'i':
+  			case 'o':
+  			case 'u':
+  				count += 1;
+  				break;
+  		}
+  	}
+  	return count;
   }
   
   public static void main (String[] args) {     
@@ -35,5 +52,9 @@ public class Function {
     System.out.print("Enter a word to check if it's a palindrome: ");
     String potentialPalindrome = input.next();
     System.out.println(function.isPalindrome(potentialPalindrome));
+    
+    System.out.print("Enter a string to count the vowels of: ");
+    String toVowelCount = input.nextLine();
+    System.out.println(function.vowelCount(toVowelCount));
   }   
 }
