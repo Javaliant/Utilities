158e320e1430a039bce91d3aba3eaab3796a3317
Luigi Vincent <luigivincenthr@gmail.com>
2014-08-17 16:26:29 -0400

    Update Function.java

--- Function.java
+++ Function.java
@@ -7,20 +7,33 @@ import java.io.*;
 
 public class Function {
   // Reversing a string
-  String reverse(String str) {  
+  public static String reverse(String str) {  
     return new StringBuilder(str).reverse().toString();
   }
   // add up all the numbers from 1 to num
-  int addTill(int num) {
+  public static int addTill(int num) {
     return num * (num + 1) / 2;
-  } 
+  }
+  // check if a word is a palindrome
+  public static boolean isPalindrome(String str) {
+  	return str.equals(new StringBuilder(str).reverse().toString());
+  }
   
   public static void main (String[] args) {     
     Scanner input = new Scanner(System.in);
     Function function = new Function();
 
     // testing functionality
-    System.out.println(function.reverse(input.nextLine()));
-    System.out.println(function.addTill(input.nextInt())); 
+    System.out.print("Enter a string to reverse: ");
+    String toBeReversed = input.nextLine();
+    System.out.println(function.reverse(toBeReversed));
+
+    System.out.print("Let's add up all the numbers from 1 to a number.\nChoose the number: ");
+    int sumInt = input.nextInt();
+    System.out.println(function.addTill(sumInt));
+
+    System.out.print("Enter a word to check if it's a palindrome: ");
+    String potentialPalindrome = input.next();
+    System.out.println(function.isPalindrome(potentialPalindrome));
   }   
 }
