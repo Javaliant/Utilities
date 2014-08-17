ffc5668b87b266e88b646f7261143823305ce5d1
Luigi Vincent <luigivincenthr@gmail.com>
2014-08-17 13:16:07 -0400

    Update Function.java

--- Function.java
+++ Function.java
@@ -1,18 +1,26 @@
 /* Author: Luigi Vincent
-Simple String Reversing
+Simple functions for use, practice, and sharing.
 */
 
 import java.util.*; 
 import java.io.*;
 
-public class Function {  
-  String Reverse(String str) {  
+public class Function {
+  // Reversing a string
+  String reverse(String str) {  
     return new StringBuilder(str).reverse().toString();
+  }
+  // add up all the numbers from 1 to num
+  int addTill(int num) {
+    return num * (num + 1) / 2;
   } 
   
   public static void main (String[] args) {     
     Scanner input = new Scanner(System.in);
+    Function function = new Function();
 
-    System.out.print(new Function().Reverse(input.nextLine())); 
+    // testing functionality
+    System.out.println(function.reverse(input.nextLine()));
+    System.out.println(function.addTill(input.nextInt())); 
   }   
 }
