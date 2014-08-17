04c1890509321c69fb5e2dde3d03ee12087258fa
Luigi Vincent <luigivincenthr@gmail.com>
2014-08-17 12:52:50 -0400

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,19 @@
+/* Author: Luigi Vincent
+Simple String Reversing
+*/
+
+import java.util.*; 
+import java.io.*;
+
+public class Function {  
+  String Reverse(String str) {  
+    return new StringBuilder(str).reverse().toString();
+  } 
+  
+  public static void main (String[] args) {     
+    Scanner input = new Scanner(System.in);
+
+    System.out.print(new Function().Reverse(input.nextLine())); 
+  }   
+  
+}
