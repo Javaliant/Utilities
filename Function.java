9a16947a24695f03ccc828ee166921f8a54a0e9e
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-07 19:35:10 -0500

    Update Function.java

--- Function.java
+++ Function.java
@@ -26,6 +26,21 @@ public class Function {
   	}
   	return largest;
   }
+	  // Check if number is prime
+	  public static boolean isPrime(long n) {
+	    	if (n == 1) { return false; }
+	    	else if (n < 4) { return true; }
+	   		else if (n % 2 == 0 || n % 3 == 0) {
+	        	return false;
+	    	} else {
+	        	for (int i = 5; i * i <= n; i += 6) {
+	            	if (n % i == 0 || n % (i + 2) == 0) {
+	                	return false;
+	            	}
+	        	}
+	        return true;
+	    	}
+	}
   // Get int array from String input, ignores non-integers and words that include integers
   public static int[] getIntegers(String s) {
     	ArrayList<Integer> helper = new ArrayList<>();
