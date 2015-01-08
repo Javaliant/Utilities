a96a1519f490312ca40183a2cb62d9ece87c5043
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-08 17:38:45 -0500

    Update Function.java

--- Function.java
+++ Function.java
@@ -41,20 +41,24 @@ public class Function {
   	}
   	return largest;
   }
-	  // Check if number is prime
-	  public static boolean isPrime(long n) {
-	    	if (n == 1) { return false; }
-	    	else if (n < 4) { return true; }
-	   		else if (n % 2 == 0 || n % 3 == 0) {
-	        	return false;
-	    	} else {
-	        	for (int i = 5; i * i <= n; i += 6) {
-	            	if (n % i == 0 || n % (i + 2) == 0) {
-	                	return false;
-	            	}
-	        	}
-	        return true;
-	    	}
+  // Check if a number is prime -- expects a positive int
+  private static boolean isPrime(int num) {
+    	if (num < 1) {
+    		throw new NumberFormatException(
+    			"Argument must be a positive integer." +
+    			"\n Argument was " + num + ".");
+    	}
+    	if (num <= 3){
+    		return num == 1 ? false : true;
+    	}
+        if (num % 2 == 0) { return false; }
+
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0) {
+            	return false;
+            }
+        }
+        return true;
 	}
   // Get int array from String input, ignores non-integers and words that include integers
   public static int[] getIntegers(String s) {
